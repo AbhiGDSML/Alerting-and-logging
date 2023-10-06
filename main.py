c46c9acd03c35241df4dc20a1eb9a7bcd1fa1864
Abhijit Gaunker <145704876+AbhiGDSML@users.noreply.github.com>
2023-10-06 10:48:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from Log import logger
+
+
+logger.info("testing info message")
+logger.warning("testing warn message")
+logger.error("testing warn message")
+logger.critical("testing warn message")
